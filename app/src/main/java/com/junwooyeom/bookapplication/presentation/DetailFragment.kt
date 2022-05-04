1e6f4b8f97ab3dee09cc3246a850e958c6479167
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-05-04 17:20:25 +0900

    [REFACTOR] Application Navigation 추가.

---
+++ app/src/main/java/com/junwooyeom/bookapplication/presentation/DetailFragment.kt
@@ -0,0 +1,8 @@
+package com.junwooyeom.bookapplication.presentation
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailFragment : Fragment(){
+}
