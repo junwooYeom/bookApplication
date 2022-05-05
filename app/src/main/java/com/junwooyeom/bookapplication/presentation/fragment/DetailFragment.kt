950deb664bc144ee3f7825526acfc6f3fb195886
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-05-05 20:58:20 +0900

    [CHORE] DetailFragment 기본 설정.

--- app/src/main/java/com/junwooyeom/bookapplication/presentation/fragment/DetailFragment.kt
+++ app/src/main/java/com/junwooyeom/bookapplication/presentation/fragment/DetailFragment.kt
@@ -1,8 +1,31 @@
 package com.junwooyeom.bookapplication.presentation.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.junwooyeom.bookapplication.R
+import com.junwooyeom.bookapplication.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : Fragment(){
+
+    private lateinit var binding: FragmentDetailBinding
+    private val args: DetailFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDetailBinding.bind(view)
+    }
 }
